4b0c2827c01df72639117ec45d78b08dae9bb3b2
deichor <deichors@gmail.com>
2025-02-13 13:59:19 +0300

    Implement quest management system with QuestHolder and abstract QuestManager class

---
+++ core/src/com/deichor/questapi/core/QuestHolder.java
@@ -0,0 +1,55 @@
+package com.deichor.questapi.core;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Collection;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.PriorityQueue;
+
+public class QuestHolder {
+    private static final Map<Integer, QuestManager> quests = new HashMap<>();
+    private static final AtomicInteger nextId = new AtomicInteger(1);
+    private static final PriorityQueue<Integer> availableIds = new PriorityQueue<>();
+
+    public static Integer addQuest(QuestManager quest) {
+        int questId;
+        if (!availableIds.isEmpty()) {
+            questId = availableIds.poll();
+        } else {
+            questId = nextId.getAndIncrement();
+        }
+        quests.put(questId, quest);
+        return questId;
+    }
+
+    public static void removeQuest(Integer questId) {
+        if (quests.remove(questId) != null) {
+            availableIds.offer(questId);
+        }
+    }
+
+    public static boolean finishQuest(Integer questId) {
+        QuestManager quest = quests.get(questId);
+        if (quest != null) {
+            if(quest.finishQuest()) {
+                removeQuest(questId);
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+
+    public static QuestManager getQuest(Integer questId) {
+        return quests.get(questId);
+    }
+
+    public static Collection<QuestManager> getAllQuests() {
+        return quests.values();
+    }
+
+    public static boolean hasQuest(Integer questId) {
+        return quests.containsKey(questId);
+    }
+
+}
