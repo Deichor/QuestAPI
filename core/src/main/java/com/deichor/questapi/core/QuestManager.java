b8b90f38e17629b4beea93f77216afad86a03dec
deichor <deichors@gmail.com>
2025-02-13 13:38:53 +0300

    Refactor QuestManager to be abstract and introduce QuestHolder for quest management

--- core/src/main/java/com/deichor/questapi/core/QuestManager.java
+++ core/src/main/java/com/deichor/questapi/core/QuestManager.java
@@ -1,17 +1,28 @@
 package com.deichor.questapi.core;
 
-import com.deichor.questapi.core.model.Quest;
+import com.deichor.questapi.core.extend.BaseQuest;
 
-public class QuestManager {
+public abstract class QuestManager {
 
-    private Quest quest;
+    private BaseQuest quest;
 
-    public QuestManager(Quest quest) {
+    public QuestManager(BaseQuest quest) {
         this.quest = quest;
         quest.start();
     }
-    
-    
-    
 
+    protected abstract void giveReward();
+
+    public boolean finishQuest(){
+        if(quest.getRequirements().stream().allMatch(requirement -> requirement.isComplete())){
+            quest.complete();
+            giveReward();
+            return true;
+        }
+        return false;
+    }
+    
+    protected BaseQuest getQuest() {
+        return quest;
+    }
 }
