a975cb2bf34ab0e87e9c5d3b6bc8d38ed2749ba5
deichor <deichors@gmail.com>
2025-02-17 01:34:17 +0300

    Add Java plugin and update Paper module build configuration

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -1,13 +1,17 @@
 plugins {
+    id("java")
     id("questapi-java21")
     id("io.papermc.paperweight.userdev") version "2.0.0-beta.14"
+    id("questapi-general")
+    id("questapi-publications")
 }
-
-group = "com.deichor.questapi.paper"
-version = "1.0-SNAPSHOT"
 
 paperweight.reobfArtifactConfiguration = io.papermc.paperweight.userdev.ReobfArtifactConfiguration.MOJANG_PRODUCTION
 
 dependencies {
     paperweight.foliaDevBundle("1.21.4-R0.1-SNAPSHOT")
+}
+
+questapiPublications {
+    artifactId = "questapi-paper"
 }
