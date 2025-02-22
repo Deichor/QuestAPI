45662fd88a5616b59f6670cda10b4646ad50b7d2
deichor <deichors@gmail.com>
2025-02-22 20:35:06 +0300

    Add implementation keyword for HikariCP dependency in build configuration

--- buildSrc/src/main/kotlin/questapi-database.gradle.kts
+++ buildSrc/src/main/kotlin/questapi-database.gradle.kts
@@ -1,8 +1,10 @@
+import gradle.kotlin.dsl.accessors._729aa7c1588b83738f7ec34c0a320432.implementation
+
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    "com.zaxxer:HikariCP:${Versions.HIKARI}"
+    implementation("com.zaxxer:HikariCP:${Versions.HIKARI}")
 }
 
